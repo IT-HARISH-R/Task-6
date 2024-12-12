e8b19a0e0607bca3510ff15652338d80e1c4a3f6
IT-HARISH-R <harishmass27.8@gmail.com>
2024-12-12 19:38:45 +0530

    add cound

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,10 +7,12 @@ const Cart = ({ cart, updateQuantity, removeFromCart }) => {
             <h1 className="text-2xl font-bold mb-4">Cart</h1>
             <h2 className="text-xl font-semibold">Total Price: ${finalPrice.toFixed(2)}</h2>
             <div>
-                {cart.map(item => (
+                {console.log(cart)}
+                {cart.map(item => (                    
                     <div key={item.id} className="border p-4 rounded shadow-md mb-4">
                         <h2 className="text-lg font-semibold">{item.title}</h2>
                         <p className="text-gray-700">${item.price} x {item.quantity}</p>
+                        <h1>Count: {item.rating.count}</h1>
                         <div className="flex gap-2 mt-2">
                             <button
                                 onClick={() => updateQuantity(item.id, 1)}
